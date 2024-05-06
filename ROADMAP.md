b009303f7bd1257bfbae56829b4a8793307cca3a
tailselrayo <rodtailselrayo@gmail.com>
2024-05-06 21:28:35 +0200

    initial commit

---
+++ ROADMAP.md
@@ -0,0 +1,22 @@
+# Isaac Lockout Project - Roadmap (messy as heck)
+
+## Basic fonctionality
+
+### TODO
+
+- Home page basic about
+- LockoutGrid component
+- Add different playmods (lockout, bingo, solo)
+- Add a challenges.ts file
+- Add types
+- Add necessary files (bosses, pickups, gamble, etc...)
+- customize /404 page
+
+### DONE
+
+## Advanced fonctionality
+
+- Custom challenges (using a /challenges page)
+- Toggable challenges
+- Theme customization
+- /about page
---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import "./app.css";
+import Nav from "./components/Nav";
+
+//I'll barely touch that if possible, looks scarily important
+
+export default function App() {
+  return (
+    <Router
+      root={(props) => (
+        <>
+          <Nav />
+          <Suspense>{props.children}</Suspense>
+        </>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  );
+}
